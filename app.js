b8aafc16779cae7039a589fc8b7396461e47ba88
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-02-28 22:21:29 +0530

    changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const MongoStore = require('connect-mongo');
 const catsRoutes=require("./routers/cat")
 const userRoutes=require("./routers/user");
 
-const dbUrl=process.env.DB_URL ||'mongodb://127.0.0.1:27017/catsDB';
+const dbUrl=process.env.DB_URL ||'mongodb://localhost:27017/catsDB';
 
 mongoose.set("strictQuery", false);
 mongoose.connect(dbUrl)
